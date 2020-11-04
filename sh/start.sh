24838bb44545326754ede2059d86a73b3652fc4b
Rostislav Nedelchev <rostislav.nedelchev@gmail.com>
2020-11-04 13:41:36 +0100

    Update start.sh

--- sh/start.sh
+++ sh/start.sh
@@ -75,7 +75,7 @@ tail -F "${NIFI_HOME}/logs/nifi-app.log" &
 "${NIFI_HOME}/bin/nifi.sh" run &
 nifi_pid="$!"
 
-trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
+# trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
 
 echo NiFi running with PID ${nifi_pid}.
 wait ${nifi_pid}

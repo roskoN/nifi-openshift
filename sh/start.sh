577d084248669b40d693388aca6e7caaa90f90d1
Rostislav Nedelchev <rostislav.nedelchev@gmail.com>
2020-11-04 14:19:26 +0100

    Update start.sh

--- sh/start.sh
+++ sh/start.sh
@@ -72,12 +72,12 @@ esac
 
 # Continuously provide logs so that 'docker logs' can    produce them
 tail -F "${NIFI_HOME}/logs/nifi-app.log" &
-"${NIFI_HOME}/bin/nifi.sh" run &
-nifi_pid="$!"
+"${NIFI_HOME}/bin/nifi.sh" run 
+#nifi_pid="$!"
 
 # trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
 
-echo NiFi running with PID ${nifi_pid}.
-wait ${nifi_pid}
+# echo NiFi running with PID ${nifi_pid}.
+# wait ${nifi_pid}
 
-while :; do sleep 86400; done
+# while :; do sleep 86400; done
